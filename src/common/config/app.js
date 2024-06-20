a58cb0f91f27c65cd55d5ffb1ea27addf11df5ed
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-06-20 17:12:19 -0400

    correccion de errores

--- src/common/config/app.js
+++ src/common/config/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const userRoutes = require('../../user/infrastructure/userRoutes'); // Asegúrate de importar las rutas de usuario
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/users', userRoutes); // Configura las rutas de usuario
 
 const port = process.env.PORT || 3000;
 
