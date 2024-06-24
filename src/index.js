ba942b097ebb452f3085a1564c3dab4c6e94f4e3
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-06-23 21:50:19 -0400

    carga a firebase correctamente

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const {app} = require('./common/config/app');
+const functions = require('firebase-functions');
+const {app} = require('./common/config/app.js');
 
-exports.users = onRequest((request, response) => {
-    logger.info("Received user request", {structuredData: true});
-    return app(request, response);
-});
+exports.api = functions.https.onRequest(app);
