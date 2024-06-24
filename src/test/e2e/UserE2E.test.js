e792b457db528624157ff1d6cd78351603d11d09
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-06-23 23:00:48 -0400

    corrigiendo errores

--- src/test/e2e/UserE2E.test.js
+++ src/test/e2e/UserE2E.test.js
@@ -1,12 +1,15 @@
 const request = require('supertest');
+require('dotenv').config(); // Cargar las variables de entorno desde el archivo .env
 
 describe('User E2E', () => {
     it('should create a new user and retrieve it', async () => {
         const user = { id: '1', name: 'John Doe', email: 'john.doe@example.com' };
+        const projectId = process.env.FIREBASE_PROJECT_ID; // Leer el ID del proyecto desde las variables de entorno
+        const baseUrl = `http://localhost:5001/${projectId}/us-central1/api`;
+        console.log(baseUrl);
+        await request(baseUrl).post('/users').send(user).expect(201);
 
-        await request('http://localhost:5001/trading-bot-ca422/us-central1/api').post('/users').send(user).expect(201);
-
-        const response = await request('http://localhost:5001/trading-bot-ca422/us-central1/api').get('/users/1');
+        const response = await request(baseUrl).get('/users/1');
         expect(response.status).toBe(200);
         expect(response.body).toMatchObject(user);
     });
