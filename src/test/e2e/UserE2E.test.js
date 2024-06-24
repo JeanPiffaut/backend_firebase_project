3e8689c7fbf9466bf8325d191c314e86b69bfa02
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-06-23 22:49:43 -0400

    testing corregido por prettier

--- src/test/e2e/UserE2E.test.js
+++ src/test/e2e/UserE2E.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 
 describe('User E2E', () => {
     it('should create a new user and retrieve it', async () => {
-        const user = {id: '1', name: 'John Doe', email: 'john.doe@example.com'};
+        const user = { id: '1', name: 'John Doe', email: 'john.doe@example.com' };
 
         await request('http://localhost:5001/trading-bot-ca422/us-central1/api').post('/users').send(user).expect(201);
 
