9ba732f125203c4caaa1d7fe6fb0851b503dd3ec
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-07-05 20:35:22 -0400

    deployment correcto

--- src/test/e2e/UserE2E.test.js
+++ src/test/e2e/UserE2E.test.js
@@ -4,7 +4,7 @@ require('dotenv').config(); // Cargar las variables de entorno desde el archivo 
 describe('User E2E', () => {
     it('should create a new user and retrieve it', async () => {
         const user = { id: '1', name: 'John Doe', email: 'john.doe@example.com' };
-        const projectId = process.env.FIREBASE_PROJECT_ID; // Leer el ID del proyecto desde las variables de entorno
+        const projectId = process.env.PROJECT_ID; // Leer el ID del proyecto desde las variables de entorno
         const baseUrl = `http://localhost:5001/${projectId}/us-central1/api`;
         await request(baseUrl).post('/users').send(user).expect(201);
 
